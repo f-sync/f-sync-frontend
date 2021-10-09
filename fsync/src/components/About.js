e21dd83d3a246791c5062b03cd2603de01b11b12
Sima <email@student.monash.edu>
2021-10-09 08:59:33 +0300

    First two slides code

--- fsync/src/components/About.js
+++ fsync/src/components/About.js
@@ -1,7 +1,16 @@
+import NavBar from "./NavBar";
 const About = () => {
     return (
-        <div>
-            <h1> This is the About Page</h1>
+        <div className="page_container">
+            <NavBar />
+            <h1 className="page_title">F-sync</h1>
+            <p className="title_description">Fashion-as-a-service</p>
+
+            <h2 className="about_title">Technology</h2>
+            <p className="about_description">A seamless, branded user experience for your customers. A
+backend system giving you full visibility. Designed to work at scale.</p>
+        
+            <h2 className="about_title">Operations</h2>
         </div>
     )
 }
---
+++ fsync/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div className="nav_container">
+            <button>Home</button>
+            <button>About</button>
+        </div>
+    )
+}
+
+export default NavBar
