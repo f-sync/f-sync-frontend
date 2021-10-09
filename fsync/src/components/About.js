2cdf520d953a4e2634c7c4439e3885b0693fe7f2
Sima <email@student.monash.edu>
2021-10-09 09:03:30 +0300

    Added text to about page

--- fsync/src/components/About.js
+++ fsync/src/components/About.js
@@ -8,9 +8,14 @@ const About = () => {
 
             <h2 className="about_title">Technology</h2>
             <p className="about_description">A seamless, branded user experience for your customers. A
-backend system giving you full visibility. Designed to work at scale.</p>
+            backend system giving you full visibility. Designed to work at scale.</p>
         
             <h2 className="about_title">Operations</h2>
+            <p className="about_description">Stock management, request products across locations, delivery &
+            returns partnerships - we’ve got it down to a science.</p> 
+            <h2 className="about_title">Data</h2>
+            <p className="about_description">Get the reporting you need to keep track of stock levels in realtime across your different sales channels</p> 
+
         </div>
     )
 }
